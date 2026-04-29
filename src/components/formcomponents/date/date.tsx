d933d02de745ba357b3c7df022e7b110831d2b69
astghik <astghik.p100@gmail.com>
2026-04-29 18:59:59 +0400

    Revert "Fixed the date-type question output style in complete results"
    This reverts commit c2307ff6f3e82846cea0ed549532511d8f5f3565.

--- src/components/formcomponents/date/date.tsx
+++ src/components/formcomponents/date/date.tsx
@@ -13,7 +13,7 @@ import itemControlConstants from '../../../constants/itemcontrol';
 import { GlobalState } from '../../../reducers';
 import { getExtension, getItemControlExtensionValue } from '../../../util/extension';
 import { evaluateFhirpathExpressionToGetDate } from '../../../util/fhirpathHelper';
-import { getSublabelText, isReadOnly } from '../../../util/index';
+import { getSublabelText } from '../../../util/index';
 import { mapStateToProps, mergeProps, mapDispatchToProps } from '../../../util/map-props';
 import { Resources } from '../../../util/resources';
 import { Path } from '../../../util/refero-core';
@@ -191,10 +191,6 @@ class DateComponent extends React.Component<Props & ValidationProps> {
           {...this.props}
         />
       );
-    }
-
-    if (this.props.pdf || isReadOnly(this.props.item)) {
-      return element || null;
     }
 
     return (
